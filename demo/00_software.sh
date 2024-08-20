27925d7c7059c151c5a8f1ab2b7f06b872af7a1d
d3v-null <dev.null@curtin.edu.au>
2024-08-20 17:34:28 +0800

    alias binaries in hybrid mode

--- demo/00_software.sh
+++ demo/00_software.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2139
 
 export docker_base="docker run --rm -it"
 export docker_img=${docker_img:="mwatelescope/mwa-demo:latest"}
@@ -12,13 +13,13 @@ else
     export docker_base="$docker_base -v \${PWD}:\${PWD} -v \${outdir}:\${outdir} -w \${PWD}"
 fi
 
-export birli="$docker_base --entrypoint=birli '${docker_img}'"
-export giant_squid="$docker_base --entrypoint=giant-squid -e MWA_ASVO_API_KEY=\$MWA_ASVO_API_KEY '${docker_img}'"
-export hyperdrive="$docker_base --entrypoint=hyperdrive -e MWA_BEAM_FILE=\$MWA_BEAM_FILE '${docker_img}'"
-export jq="$docker_base --entrypoint=jq '${docker_img}'"
-export wsclean="$docker_base --entrypoint=wsclean '${docker_img}'"
-export python="$docker_base --entrypoint=python '${docker_img}'"
-export plot_calqa="$docker_base --entrypoint=plot_calqa.py '${docker_img}'"
-export plot_prepqa="$docker_base --entrypoint=plot_prepvisqa.py '${docker_img}'"
-export run_calqa="$docker_base --entrypoint=run_calqa.py '${docker_img}'"
-export run_prepqa="$docker_base --entrypoint=run_prepvisqa.py '${docker_img}'"
+alias birli="$docker_base --entrypoint=birli '${docker_img}'"
+alias giant_squid="$docker_base --entrypoint=giant-squid -e MWA_ASVO_API_KEY=\$MWA_ASVO_API_KEY '${docker_img}'"
+alias hyperdrive="$docker_base --entrypoint=hyperdrive -e MWA_BEAM_FILE=\$MWA_BEAM_FILE '${docker_img}'"
+alias jq="$docker_base --entrypoint=jq '${docker_img}'"
+alias wsclean="$docker_base --entrypoint=wsclean '${docker_img}'"
+alias python="$docker_base --entrypoint=python '${docker_img}'"
+alias plot_calqa="$docker_base --entrypoint=plot_calqa.py '${docker_img}'"
+alias plot_prepqa="$docker_base --entrypoint=plot_prepvisqa.py '${docker_img}'"
+alias run_calqa="$docker_base --entrypoint=run_calqa.py '${docker_img}'"
+alias run_prepqa="$docker_base --entrypoint=run_prepvisqa.py '${docker_img}'"
