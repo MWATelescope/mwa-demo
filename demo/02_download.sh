ce74f6b78a97187edc3927949d8a883c4ccb953d
d3v-null <dev.null@curtin.edu.au>
2024-08-21 09:22:27 +0800

    newlines after download instructions

--- demo/02_download.sh
+++ demo/02_download.sh
@@ -18,6 +18,8 @@ export obsids_csv=${obsids_csv:-"${outdir}/obsids.csv"}
 
 if [ ! -f "$obsids_csv" ]; then
     echo "obsids_csv=$obsids_csv does not exist. try: ${SCRIPT_BASE}/01_tap.sh" && exit 1
+else
+    echo "there are $(wc -l $obsids_csv | awk '{print $1}') obsids in $obsids_csv"
 fi
 
 if [ -z $MWA_ASVO_API_KEY ]; then
@@ -32,18 +34,25 @@ fi
 echo "you should run one of the following commands manually:"
 echo " -> request raw visibilities"
 echo "giant_squid submit-vis $obsids_csv"
+echo ""
+
 echo " -> (or) submit preprocessed visibility conversion jobs (uvfits or ms)"
 echo "giant_squid submit-conv $obsids_csv -p output=uvfits,avg_freq_res=40,avg_time_res=2,flag_edge_width=80"
+echo ""
 
 echo " -> get human-readable list of jobs that are queued or processing"
 echo "giant_squid list $obsids_csv"
+echo ""
 
 echo " -> (advanced) get a machine readable list of jobs that are queued or processing"
 echo "giant_squid list $obsids_csv --states queued,processing --json >$outdir/jobs.json"
 echo "jq -r '[.[]|.jobId]|join(" ")' $outdir/jobs.json"
+echo ""
 
 echo " -> wait until a job is ready"
 echo "giant_squid wait $obsids_csv"
+echo ""
 
 echo " -> download a job"
 echo "giant_squid download jobid"
+echo ""
