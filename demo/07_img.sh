378b8624f66b36a0e71843057ea909fc51fd8938
Dev <dev.null@curtin.edu.au>
2024-10-01 14:32:15 +0800

    update image script

--- demo/07_img.sh
+++ demo/07_img.sh
@@ -8,12 +8,12 @@ if [ -n "$ZSH_VERSION" ]; then ME="${0:A}"; else ME=$(realpath ${BASH_SOURCE:0})
 export SCRIPT_BASE=${SCRIPT_BASE:-$(dirname $ME)}
 source "$SCRIPT_BASE/00_env.sh"
 
-export obsid=${obsid:-1341914000}
-
+export obsid=${obsid:-1121334536}
 # check for calibrated measurement set from previous step
 export cal_ms="${cal_ms:-${outdir}/${obsid}/cal/hyp_cal_${obsid}.ms}"
 
 set -e
+
 if (($(ls -ld $cal_ms | wc -l) < 1)); then
     echo "cal_ms=$cal_ms does not exist. trying 06_cal.sh"
     $SCRIPT_BASE/06_cal.sh
@@ -22,7 +22,26 @@ fi
 # ### #
 # IMG #
 # ### #
+export briggs=${briggs:-0}
+export scale=${scale:-"20asec"}
+export mgain=${mgain:-0.85}
+export mscale=${mscale:-0.6}
+export size=${size:-2048}
+export gain=${gain:-0.1}
+
+cores=$(nproc --all)
+export cpus=${cpus:-$cores}
+
+export taper=${taper:-}
+export multiscale=${multiscale:-}
+
+if [[ -n $multiscale ]]; then
+    multiscale="-multiscale -multiscale-scale-bias=${mscale}"
+fi
+
 mkdir -p "${outdir}/${obsid}/img"
+export imname=${imname:-${obsid}}
+export imgname="${outdir}/${obsid}/img/${imname}"
 
 # wsclean needs to know the directory of the beam file
 export beam_path="${MWA_BEAM_FILE%/*}"
@@ -34,24 +53,25 @@ if [[ -n "${gpus:-}" ]]; then
     export idg_mode="hybrid"
 fi
 
-export imgname="${outdir}/${obsid}/img/wsclean_hyp_${obsid}"
-if [ ! -f "${imgname}-image.fits" ]; then
+if [ ! -f "${imgname}-image-pb.fits" ]; then
     wsclean \
         -name "${imgname}" \
-        -size 2048 2048 \
-        -scale 20asec \
-        -pol i \
-        -niter 100 \
+        -size ${size} ${size} \
+        -j $cpus \
+        -scale ${scale} \
+        -pol I \
+        -nmiter 2 \
+        -niter 10000 \
+        -mgain ${mgain} \
         -gridder idg -grid-with-beam -idg-mode $idg_mode \
-        -multiscale \
-        -weight briggs 0 \
-        -mgain 0.85 -gain 0.1 \
+        -weight briggs ${briggs} \
+        ${multiscale} ${taper} \
+        -gain ${gain} \
         -auto-threshold 1 -auto-mask 3 \
         -make-psf \
-        -circular-beam \
         -mwa-path "$beam_path" \
         -temp-dir /tmp \
         $cal_ms
 else
-    echo "${imgname}-image.fits exists, skipping wsclean"
+    echo "${imgname}-image-pb.fits exists, skipping wsclean"
 fi
