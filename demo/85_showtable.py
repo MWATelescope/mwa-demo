cbafe9a9f3ccdc5d4afedc67846e325910e187bb
d3v-null <dev.null@curtin.edu.au>
2025-12-10 21:42:08 +0800

    rm bp

--- demo/85_showtable.py
+++ demo/85_showtable.py
@@ -8,6 +8,5 @@ import sys
 print(''.join(taql(f"show table '{sys.argv[-1]}'")))
 times = Time(np.unique(taql(f"select TIME from '{sys.argv[-1]}'").getcol("TIME")) / 86400.0, format="mjd", scale="utc")
 print(f"times ({len(times)}):\n{times.iso}")
-breakpoint()
 
 # print()
