2b8887b2658d3351dd7c12f645399672cb955c13
Dev <dev.null@curtin.edu.au>
2026-01-22 09:09:14 +0800

    showtable: stman info

--- demo/85_showtable.py
+++ demo/85_showtable.py
@@ -6,7 +6,14 @@ import numpy as np
 import sys
 
 print(''.join(taql(f"show table '{sys.argv[-1]}'")))
-times = Time(np.unique(taql(f"select TIME from '{sys.argv[-1]}'").getcol("TIME")) / 86400.0, format="mjd", scale="utc")
+
+t = taql(f"select * from '{sys.argv[-1]}'")
+print("\nData Manager Info:")
+dminfo = t.getdminfo()
+for name, info in dminfo.items():
+    print(f"  {name}: {info['TYPE']} (columns: {', '.join(info['COLUMNS'])})")
+times = Time(np.unique(t.getcol("TIME")) / 86400.0, format="mjd", scale="utc")
 print(f"times ({len(times)}):\n{times.iso}")
 
-# print()
+history = taql(f"select * from '{sys.argv[-1]}/HISTORY'").getcol("MESSAGE")
+print(f"history ({len(history)}):\n{history}")
